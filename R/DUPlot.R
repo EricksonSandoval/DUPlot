4bed7419fe309a9f61f3722a6586dbf5e8d6be9b
EricksonSandoval <52221347+EricksonSandoval@users.noreply.github.com>
2019-07-02 10:57:04 -0500

    Add files via upload

--- R/DUPlot.R
+++ R/DUPlot.R
@@ -7,11 +7,13 @@
 #' @param PAIS
 #' @param EXPORTAR
 #' @param RUTA
-#'
+#' @param DATABASE
+#' @param INTERV
+#' @param TITLE
+#' @param PATH_FILE
 #'
 #' @export
 #' @examples
-#' AccyAnalysis()
 
 
 calc_performance <- function(DF_TOTAL, PAIS, EXPORTAR, RUTA){
@@ -436,8 +438,8 @@ SankeyDiagram <- function(DATABASE, INTERV, TITLE, PATH_FILE){
     geom_stratum(color = NA, width=0.45) +
     scale_fill_identity() +
     ggtitle(TITLE) +
-    labs(subtitle = paste0("\nCAMPAÑA ", min(as.character(DATABASE$CAMPANA)),
-                           " - CAMPAÑA ", max(as.character(DATABASE$CAMPANA)))) +
+    labs(subtitle = paste0("\nCAMPA?A ", min(as.character(DATABASE$CAMPANA)),
+                           " - CAMPA?A ", max(as.character(DATABASE$CAMPANA)))) +
     geom_text(stat = "stratum", fontface = "bold", color = "black", size=5)
 
   png(PATH_FILE, width=4500, height=3000, res=300)
